9e45cd177170afb1baca43314e6f647e457e1bb2
Fatemeh <fatemeh.yousefi.c71@gmail.com>
2026-07-29 22:06:57 +0200

    build_function_models() added

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-from src.parser import parse_json
+from src.parser import parse_json, build_function_models
 
 print("Call Me Maybe started!")
 
 functions = parse_json(Path("data/input/functions_definition.json"))
 promts = parse_json(Path("data/input/function_calling_tests.json"))
-
+function_models = build_function_models(functions)
 
 print(f"Loaded {len(functions)} functions.")
 print(f"Loaded {len(promts)} promts.")
+for function in function_models:
+    print(function)
--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
-from .models import FunctionDefinition
+from .models import FunctionDefinition, Parameter
+from typing import List
 
 
 def parse_json(file_path: Path):
@@ -8,5 +9,30 @@ def parse_json(file_path: Path):
         return json.load(file)
 
 
-def build_functions_models(data: list[dict]) -> list[FunctionDefinition]:
-    pass
+def build_function_models(data: list[dict]) -> list[FunctionDefinition]:
+    function_models: List[FunctionDefinition] = []
+
+    for function_dict in data:
+        name = function_dict["name"]
+        description = function_dict["description"]
+        return_type = function_dict["returns"]["type"]
+
+        parameters: List[Parameter] = []
+
+        for key, value in function_dict["parameters"].items():
+            parameter = Parameter(
+                name=key,
+                type=value["type"]
+            )
+            parameters.append(parameter)
+
+        function_model = FunctionDefinition(
+            name=name,
+            description=description,
+            parameters=parameters,
+            return_type=return_type
+        )
+
+        function_models.append(function_model)
+
+    return function_models
